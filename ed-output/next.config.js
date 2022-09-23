0a2b231b1d26f325ad49644d90c326d3ae95cf24
LeeHyoJae <timelyhyo92@gmail.com>
2022-09-23 11:22:47 +0900

    Refactor: remove output

--- ed-output/next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-module.exports = {
-  productionBrowserSourceMaps: true,
-  publicRuntimeConfig: {
-    version: '1.0.0',
-    envMode: process.env.ENV_MODE,
-    forceAPITargetURL: process.env.FORCE_API_TARGET_URL,
-  },
-};
