4b375a268083319853a28694b8922ec117c2ba67
Kim Yeo Kyoung <39088247+chochozabe@users.noreply.github.com>
2022-09-26 19:51:37 +0900

    Modify: Add SFTP (#9)
    * Modify: Add SFTP
    * Modify: Get server config from init data

--- src/modules/SSH.ts
+++ src/modules/SSH.ts
@@ -1,71 +1,98 @@
-import { Client, ClientErrorExtensions } from "ssh2";
+import { Client, ClientErrorExtensions, SFTPWrapper } from "ssh2";
 import { readFileSync } from "fs";
 
 interface ISSHConfig {
-	host: string;
-	port: number | 22;
-	username: string;
-	password?: string;
-	privateKey?: Buffer;
+  host: string;
+  port: number | 22;
+  username: string;
+  password?: string;
+  privateKey?: Buffer;
 }
 
 interface ISSH {
-	_client: Array<Client> | null;
-	closeAllConnections: Function;
+  _client: Array<IClient> | null;
+  closeAllConnections: Function;
+}
+
+interface IClient {
+  connection: Client | null;
+  sftp: SFTPWrapper | null;
 }
 
 const SSH = function (this: any, configList: Array<ISSHConfig>) {
-	return new Promise<ISSH>((resolve, reject) => {
-		this._client = configList.map((config) => {
-			const conn = new Client();
-			conn
-				.once("ready", (err: Error & ClientErrorExtensions) => {
-					if (err) {
-						this._client = null;
-						return reject(err);
-					}
-					console.log(`${config.host}:${config.port} is connect`);
-				})
-				.once("close", () => {
-					console.log(`${config.host}:${config.port} is closed`);
-				})
-				.once("error", (err: Error) => {
-					console.log(`${config.host}:${config.port} cannot connected`);
-					this._client = null;
-					return reject(err);
-				})
-				.connect(config);
+  return new Promise<ISSH>((resolve, reject) => {
+    this._client = configList.map((config) => {
+      const conn = new Client();
+      const tmpClient: IClient = {
+        connection: null,
+        sftp: null,
+      };
 
-			return conn;
-		});
+      conn
+        .once("ready", (err: Error & ClientErrorExtensions) => {
+          if (err) {
+            this._client = null;
+            return reject(err);
+          }
 
-		resolve(this);
-	});
+          conn.sftp((err, sftp) => {
+            if (err) {
+              this._client = null;
+              return reject(err);
+            }
+            tmpClient.sftp = sftp;
+          });
+
+          console.log(`${config.host}:${config.port} is connect`);
+        })
+        .once("close", () => {
+          console.log(`${config.host}:${config.port} is closed`);
+        })
+        .once("error", (err: Error) => {
+          console.log(`${config.host}:${config.port} cannot connected`);
+          this._client = null;
+          return reject(err);
+        })
+        .connect(config);
+
+      tmpClient.connection = conn;
+
+      return tmpClient;
+    });
+
+    resolve(this);
+  });
 };
 
-SSH.prototype.closeAllConnections = function() {
-	return Promise.all(this._client.map((c:Client) => c.end()));
-}
+SSH.prototype.closeAllConnections = function () {
+  return Promise.all(
+    this._client.map((c: IClient) => {
+      c.connection?.end();
+    }),
+  );
+};
 
 const connection = async ({ module }: { module: boolean }) => {
-	const configList : Array<ISSHConfig> = [
-		{
-			host: "3.37.232.213",
-			port: 22,
-			username: "ec2-user",
-			privateKey: readFileSync(
-				"/Users/a220403/Documents/Key/dev-platformteam-server-key.pem",
-			),
-		},
-	];
+  const initFile = `${process.cwd()}/easy-deploy.json`;
+  const config: any = JSON.parse(readFileSync(initFile, "utf-8"));
 
-	if (module) {
-		const client: ISSH = await new (SSH as any)(configList);
+  const sshConfig: Array<ISSHConfig> = config.server.map((s: any) => {
+    return {
+      host: s.host,
+      port: s.port,
+      username: s.username,
+      password: s.password,
+      privateKey: readFileSync(s.pemLocation),
+    };
+  });
 
-		setTimeout(async () => {
-			await client.closeAllConnections();
-		}, 3000);
-	}
+  if (module) {
+    const client: ISSH = await new (SSH as any)(sshConfig);
+
+    setTimeout(async () => {
+      await client.closeAllConnections();
+    }, 3000);
+  }
 };
 
 export { SSH, connection };
