9c254029fa4f6e79022e71a5465298c31df72812
miniddo <kg3718@naver.com>
2022-10-04 18:24:10 +0900

    feat: backup 폴더 만들기, 기존 node_mouldes 폴더 사용 함수 작업

--- src/modules/backup.ts
+++ src/modules/backup.ts
@@ -1,71 +1,58 @@
-import { readFileSync } from 'fs';
-import { ISSH, SSH } from './upload';
+import { ISSH } from './upload';
 import { IDefaultDeployServerInfo } from '../commands/Init';
-
-interface ISSHConfig {
-  host: string;
-  port: number;
-  username: string;
-  password?: string;
-  privateKey?: Buffer;
-}
 
 /*
 =====================================================================================
 
                                   MODULE. BACKUP
-                                September 27, 2022
+                                 October 04, 2022
 
-Description : When Deploying, if there are deploy file already in use, a backup folder 
+Description : When Deploying, if there are bundle folder already in use, a backup folder 
 is created and managed.
 =====================================================================================
 */
-async function backup({ appName }: { appName: string }) {
-  const initDeployFile = readFileSync('./easy-deploy.json', 'utf8');
-  const initDeployJSON = JSON.parse(initDeployFile);
 
-  /**
-   * Set SSH config for connecting remote server https://www.npmjs.com/package/ssh2
-   * @param {IDefaultDeployServerInfo} serverInfo - is remote server information
-   * @returns sshConfig - consists of host, port, username, password, privateKey
-   */
-  const setSSHConfig = (serverInfo: IDefaultDeployServerInfo) => {
-    const { host, port, username, password, pemLocation } = serverInfo;
-    const sshConfig: ISSHConfig = { host, port, username };
-    if (password) sshConfig.password = password;
-    else sshConfig.privateKey = readFileSync(pemLocation);
-    return sshConfig;
-  };
-
-  /**
-   * Generate backup folder for rollback at any time
-   * @param {ISSH} client - is Client instance of ssh2 package
-   * @param {IDefaultDeployServerInfo} serverInfo - is remote server information
-   */
-  const generateBackUpFolder = async (client: ISSH, serverInfo: IDefaultDeployServerInfo) => {
-    const { deploymentDir } = serverInfo;
-    // check deploy file already is use & backup folder
-    const { stdout: hasDeployFile } = await client.exec(`find ${deploymentDir}/${appName} -name ecosystem.config.js`);
-    if (hasDeployFile) {
-      const { stdout: hasBackUpFolder } = await client.exec(`find ${deploymentDir}/ -name backup -type d`);
-      if (!hasBackUpFolder) await client.exec('mkdir backup');
-      // move the existing deploy file to the backup folder
-      await client.exec(`mv ${deploymentDir}/${appName}/* ${deploymentDir}/backup`);
-      // unzip the folder to be deployed
-      await client.exec(`tar -xvf ${appName}.tar ${appName}`);
-      // move the node_modules folder to the folder where will be deployed
-      await client.exec(`mv ${deploymentDir}/backup/node_modules ${deploymentDir}/${appName}`);
-      console.log('Generate backup folder');
+/**
+ * Generate backup folder for rollback at any time
+ * @param {ISSH} client - is Client instance of ssh2 package https://www.npmjs.com/package/ssh2
+ * @param {string} appName - is name for pm2
+ * @param {IDefaultDeployServerInfo} serverInfo - is remote server information
+ */
+const generateBackUpFolder = async (client: ISSH, appName: string, serverInfo: IDefaultDeployServerInfo) => {
+  const { deploymentDir } = serverInfo;
+  try {
+    // check bundle folder already is use & backup folder
+    const { stdout: hasBundleFile } = await client.exec(`find ${deploymentDir}/ -name ${appName}.tar`);
+    if (hasBundleFile) {
+      const { stdout: hasBackupFolder } = await client.exec(`find ${deploymentDir}/ -name backup -type d`);
+      if (!hasBackupFolder) await client.exec('mkdir backup');
+      // copy the existing bundle file to the backup folder
+      await client.exec(`cp ${deploymentDir}/${appName}/* ${deploymentDir}/backup`);
+      await client.exec(`cp -r ${deploymentDir}/${appName}/node_modules ${deploymentDir}/backup`);
+      console.log('Check bundle file & Generate backup folder');
     } else {
-      console.log('Not exist deploy file!');
+      console.log('Not exist bundle file');
     }
-  };
-
-  // server multiple instance
-  for (const serverInfo of initDeployJSON.server) {
-    const sshConfig = setSSHConfig(serverInfo);
-    const client: ISSH = await new (SSH as any)(sshConfig);
-    await generateBackUpFolder(client, serverInfo);
+  } catch (err) {
+    console.log(`SSH2 Server Error: ${(err as Error).message}`);
   }
-}
-export { backup };
+};
+
+/**
+ * Use Existing node_modules folder
+ * @param {ISSH} client - is Client instance of ssh2 package https://www.npmjs.com/package/ssh2
+ * @param {string} appName - is name for pm2
+ * @param {IDefaultDeployServerInfo} serverInfo - is remote server information
+ */
+const useExistingNodeModules = async (client: ISSH, appName: string, serverInfo: IDefaultDeployServerInfo) => {
+  const { deploymentDir } = serverInfo;
+  try {
+    // move the node_modules folder to the folder where will be deployed
+    await client.exec(`mv ${deploymentDir}/backup/node_modules ${deploymentDir}/${appName}`);
+    console.log('Use exist node_modules folder');
+  } catch (err: any) {
+    console.log(`SSH2 Server Error: ${(err as Error).message}`);
+  }
+};
+
+export { generateBackUpFolder, useExistingNodeModules };
