7da5d06943d175f91194e6842c3a243c1418e204
miniddo <kg3718@naver.com>
2022-10-17 14:17:18 +0900

    modify: file path config add.

--- src/modules/common/parseJsonFile.ts
+++ src/modules/common/parseJsonFile.ts
@@ -1,14 +1,25 @@
 import { readFileSync } from 'fs';
 
 /**
- * Parsing the easy-deploy file as json
- * @returns easy-deploy.json
+ * Parsing file as json
+ * @param {string} path - is file location for reading
+ * @returns parsed json
  */
-const parseInitJsonFile = () => {
-  const initDefaultFile = readFileSync(`${process.cwd()}/easy-deploy.json`, 'utf8');
-  const initDefaultJson: IDefaultInitInfo = JSON.parse(initDefaultFile);
+const parseJsonFile = (path: string) => {
+  const fileData = readFileSync(path, 'utf8');
+  return JSON.parse(fileData);
+}
 
+/**
+ * Paring easy-deploy.json file as json
+ * @param {string} config - is user defined easy-deploy file location
+ * @returns init parsed json
+ */
+const getInitJsonFile = (config: string) => {
+  const filePath = config ?? `${process.cwd()}/easy-deploy.json`;
+  const initDefaultJson = parseJsonFile(filePath);
   return initDefaultJson;
-};
+}
 
-export { parseInitJsonFile };
+
+export { parseJsonFile, getInitJsonFile };
