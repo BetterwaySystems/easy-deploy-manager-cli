aa29748db1b631eddf8281a2fa7ef18a96e1aba1
miniddo <57120695+miniddo@users.noreply.github.com>
2022-10-18 15:05:16 +0900

    Feat: Parsing the init json file Function add. (#15)
    * feat: parsing init deploy file as json #CU-34pexrp[PULL REQUEST]
    * modify: file path config add.
    * modify: exception handling

---
+++ src/modules/common/parseJsonFile.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+
+/**
+ * Parsing file as json
+ * @param {string} path - is file location for reading
+ * @returns parsed json
+ */
+const parseJsonFile = (path: string) => {
+  try { 
+    const fileData = readFileSync(path, 'utf8');
+    return JSON.parse(fileData);
+  } catch(err) {
+    throw err;
+  }
+}
+
+/**
+ * Paring easy-deploy.json file as json
+ * @param {string} config - is user defined easy-deploy file location
+ * @returns init parsed json
+ */
+const getInitJsonFile = (config: string) => {
+  try {
+    const filePath = config ?? `${process.cwd()}/easy-deploy.json`;
+    const initDefaultJson = parseJsonFile(filePath);
+    return initDefaultJson;
+  } catch(err) {
+    throw err;
+  }
+}
+
+
+export { parseJsonFile, getInitJsonFile };
