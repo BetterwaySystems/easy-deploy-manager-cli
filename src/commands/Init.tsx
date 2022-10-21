211d4994def3682ceec44fa076c2e2a1579f6434
LeeHyoJae <timelyhyo92@gmail.com>
2022-10-21 11:39:31 +0900

    Feat: get Config

--- src/commands/Init.tsx
+++ src/commands/Init.tsx
@@ -1,151 +1,151 @@
-import React, { useState } from 'react';
-import { Text, Box, Newline } from 'ink';
-import Markdown from 'ink-markdown';
-import dedent from 'dedent';
-import fs from 'fs';
-import { InitDescription, InitDefaultInputComponent } from '../components';
+import React, { useState } from "react";
+import { Text, Box, Newline } from "ink";
+import Markdown from "ink-markdown";
+import dedent from "dedent";
+import fs from "fs";
+import { InitDescription, InitDefaultInputComponent } from "../components";
 
 const frameworkList = [
   {
-    label: 'next',
-    value: 'next',
+    label: "next",
+    value: "next",
   },
   {
-    label: 'nest',
-    value: 'nest',
+    label: "nest",
+    value: "nest",
   },
 ];
 const packageManagerList = [
   {
-    label: 'npm',
-    value: 'npm',
+    label: "npm",
+    value: "npm",
   },
   {
-    label: 'yarn',
-    value: 'yarn',
+    label: "yarn",
+    value: "yarn",
   },
   {
-    label: 'pnpm',
-    value: 'pnpm',
+    label: "pnpm",
+    value: "pnpm",
   },
 ];
 const serverOSList = [
   {
-    label: 'amazonlinux',
-    value: 'amazonlinux',
+    label: "amazonlinux",
+    value: "amazonlinux",
   },
   {
-    label: 'ubuntu',
-    value: 'ubuntu',
+    label: "ubuntu",
+    value: "ubuntu",
   },
   {
-    label: 'centos',
-    value: 'centos',
+    label: "centos",
+    value: "centos",
   },
 ];
 const pm2ExecModeList = [
   {
-    label: 'fork',
-    value: 'fork',
+    label: "fork",
+    value: "fork",
   },
   {
-    label: 'cluster',
-    value: 'cluster',
+    label: "cluster",
+    value: "cluster",
   },
 ];
 const initSettingInfo: Array<IInitSettingForComponent> = [
   {
-    type: 'selectInput',
-    target: 'buildType',
-    label: 'buildType',
+    type: "selectInput",
+    target: "buildType",
+    label: "buildType",
     rangeNum: -1,
     itemList: frameworkList,
   },
   {
-    type: 'selectInput',
-    target: 'packageManager',
-    label: 'packageManager',
+    type: "selectInput",
+    target: "packageManager",
+    label: "packageManager",
     rangeNum: 0,
     itemList: packageManagerList,
   },
   {
-    type: 'textInput',
-    target: 'appName',
-    label: 'appName',
+    type: "textInput",
+    target: "appName",
+    label: "appName",
     rangeNum: 1,
   },
   {
-    type: 'textInput',
-    target: 'nodeVersion',
-    label: 'nodeVersion',
+    type: "textInput",
+    target: "nodeVersion",
+    label: "nodeVersion",
     rangeNum: 2,
   },
   {
-    type: 'selectInput',
-    target: 'server.os',
-    label: '(server) os',
-    defaultValue: 'amazonlinux',
+    type: "selectInput",
+    target: "server.os",
+    label: "(server) os",
+    defaultValue: "amazonlinux",
     itemList: serverOSList,
     rangeNum: 3,
   },
   {
-    type: 'textInput',
-    target: 'server.host',
-    label: '(server) host',
+    type: "textInput",
+    target: "server.host",
+    label: "(server) host",
     rangeNum: 4,
   },
   {
-    type: 'textInput',
-    target: 'server.port',
-    label: '(server) port',
-    defaultValue: '22',
+    type: "textInput",
+    target: "server.port",
+    label: "(server) port",
+    defaultValue: "22",
     rangeNum: 5,
   },
   {
-    type: 'textInput',
-    target: 'server.username',
-    label: '(server) username',
-    defaultValue: 'ec2-user',
+    type: "textInput",
+    target: "server.username",
+    label: "(server) username",
+    defaultValue: "ec2-user",
     rangeNum: 6,
   },
   {
-    type: 'textInput',
-    target: 'server.password',
-    label: '(server) password',
+    type: "textInput",
+    target: "server.password",
+    label: "(server) password",
     rangeNum: 7,
   },
   {
-    type: 'textInput',
-    target: 'server.deploymentDir',
-    label: '(server) deploymentDir',
-    defaultValue: '/home/ec2-user',
+    type: "textInput",
+    target: "server.deploymentDir",
+    label: "(server) deploymentDir",
+    defaultValue: "/home/ec2-user",
     rangeNum: 8,
   },
   {
-    type: 'textInput',
-    target: 'server.pemLocation',
-    label: '(server) pemLocation',
+    type: "textInput",
+    target: "server.pemLocation",
+    label: "(server) pemLocation",
     rangeNum: 9,
   },
   {
-    type: 'textInput',
-    target: 'server.alias',
-    label: '(server) alias',
+    type: "textInput",
+    target: "server.alias",
+    label: "(server) alias",
     rangeNum: 10,
   },
   {
-    type: 'selectInput',
-    target: 'pm2.exec_mode',
-    label: '(pm2) exec_mode',
+    type: "selectInput",
+    target: "pm2.exec_mode",
+    label: "(pm2) exec_mode",
     itemList: pm2ExecModeList,
-    defaultValue: 'fork',
+    defaultValue: "fork",
     rangeNum: 11,
   },
   {
-    type: 'textInput',
-    target: 'pm2.instance',
-    label: '(pm2) instance',
-    defaultValue: '1',
+    type: "textInput",
+    target: "pm2.instance",
+    label: "(pm2) instance",
+    defaultValue: "1",
     rangeNum: 12,
   },
 ];
@@ -163,25 +163,27 @@ ially.
 const Init = () => {
   const [step, setStep] = useState<number>(0);
   const [defaultInitInfo, setDefaultInitInfo] = useState<IDefaultInitInfo>({
-    buildType: '',
-    packageManager: '',
-    appName: '',
-    nodeVersion: '(Enter the node version used in your project)',
+    buildType: "",
+    packageManager: "",
+    appName: "",
+    nodeVersion: "(Enter the node version used in your project)",
     server: {
-      os: 'amazonlinux',
-      host: '(Enter the remote server host)',
+      os: "amazonlinux",
+      host: "(Enter the remote server host)",
       port: 22,
-      username: 'ec2-user',
-      password: '(If you need a password to access the remote server, enter it)',
-      deploymentDir: '/home/ec2-user',
-      pemLocation: '(Enter the local path where the pem file is located)',
-      alias: '(Enter the server connection alias)',
+      username: "ec2-user",
+      password:
+        "(If you need a password to access the remote server, enter it)",
+      deploymentDir: "/home/ec2-user",
+      pemLocation: "(Enter the local path where the pem file is located)",
+      alias: "(Enter the server connection alias)",
     },
     pm2: {
-      exec_mode: 'fork',
-      instance: '1',
+      exec_mode: "fork",
+      instance: "1",
     },
     env: {},
+    output: "",
   });
 
   if (step > initSettingInfo.length - 1) {
@@ -194,7 +196,7 @@ const Init = () => {
     };
 
     // create easy-deploy.json
-    fs.writeFileSync('easy-deploy.json', JSON.stringify(initializeFileInfo));
+    fs.writeFileSync("easy-deploy.json", JSON.stringify(initializeFileInfo));
   }
 
   const checkFinalJson = dedent`
@@ -241,7 +243,7 @@ const Init = () => {
             <Markdown>{checkFinalJson}</Markdown>
             <Newline />
             <Text>
-              ✨ About to write to{' '}
+              ✨ About to write to{" "}
               <Text color="#f2cd7c" bold>
                 easy-deploy.json
               </Text>
