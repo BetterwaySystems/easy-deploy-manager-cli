2ac99347f992a0a49c9715e51cca484fe700d0df
miniddo <kg3718@naver.com>
2022-11-10 18:14:14 +0900

    modify: bundle - change nest file(#3837mur)

--- src/modules/bundler/nest.ts
+++ src/modules/bundler/nest.ts
@@ -1,46 +1,29 @@
 import { ChildProcess, exec as childExec } from 'child_process';
-import { readdirSync } from 'fs';
-import appLocationDirectory from "../common/appLocationDirectory";
 import ts from 'typescript';
 import CommandBuilder from '../common/commandBuilder';
 
 const NestBundler = function (this: any, config: any, bundleOptions:Record<string, any>): any {
-  const PACKAGE_NPM: string = 'cp package.json package-lock.json';
-  const PACKAGE_YARN: string = 'cp package.json yarn.lock';
-  const PACKAGE_PNPM: string = 'cp package.json pnpm-lock.yaml';
-  const OUT_PUT_DIR: string = config.output;
-  const BUNDLE_FILE: string = 'bundle.tar';
-  const Command = new CommandBuilder();
+  const PACKAGE_NPM = 'package.json package-lock.json tsconfig.build.json';
+  const PACKAGE_YARN = 'package.json yarn.lock tsconfig.build.json';
+  const PACKAGE_PNPM = 'package.json pnpm-lock.yaml tsconfig.build.json';
+
+  const OUT_PUT_DIR = config.output;
+  const BUNDLE_FILE = 'bundle.tar';
+
   const { writedEcosystemLocationInfo, output } = bundleOptions;
+  const { packageManager } = config;
+
   const findManager = (): string => {
-    if (config.packageManager) {
-      switch (config.packageManager) {
-        case 'npm':
-          return PACKAGE_NPM;
-        case 'yarn':
-          return PACKAGE_YARN;
-        case 'pnpm':
-          return PACKAGE_PNPM;
-        default:
-          return PACKAGE_NPM;
-      }
-    } else {
-      const files = readdirSync(config.appLocation);
-      if (files === undefined) {
-        console.log(`cannot find PakageManager, Assumed to be npm`);
+    switch(packageManager) {
+      case 'npm':
         return PACKAGE_NPM;
-      } else {
-        if (files.findIndex((filename) => filename === 'yarn.lock') > -1) {
-          console.log(`PackageManager is YARN, start to collect dependencies files`);
-          return 'package.json yarn.lock tsconfig.build.json';
-        } else if (files.findIndex((filename) => filename === 'pnpm-lock.yaml') > -1) {
-          console.log(`PackageManager is PNPM, start to collect dependencies files`);
-          return 'package.json pnpm-lock.yaml tsconfig.build.json';
-        } else {
-          console.log(`PackageManager is NPM, start to collect dependencies files`);
-          return 'package.json package-lock.json tsconfig.build.json';
-        }
-      }
+      case 'yarn':
+        return PACKAGE_YARN;
+      case 'pnpm':
+        return PACKAGE_PNPM;
+      default:
+        // 초기버전은 npm만 지원, config 파일에 packageManager 없기 때문에 default는 npm으로 설치
+        return PACKAGE_NPM;
     }
   }
 
@@ -63,6 +46,9 @@ const NestBundler = function (this: any, config: any, bundleOptions:Record<strin
     const moveExternalCommand = `cd ${writedEcosystemLocationInfo.pwd}`;
     const removeTarCommand = `tar rvf ${output}/${BUNDLE_FILE} ${writedEcosystemLocationInfo.fileName}`;
     const removeEcoCommand = `rm -rf ${writedEcosystemLocationInfo.pwd}/${writedEcosystemLocationInfo.fileName}`;
+
+    const Command = new CommandBuilder();
+    
     Command
       .add(moveLocalCommand)
       .add(tarCommand)
