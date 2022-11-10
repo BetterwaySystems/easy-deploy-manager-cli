b3e2ae1f0c41384ff2c19ca964d2a93bdc505e6b
miniddo <kg3718@naver.com>
2022-11-10 17:28:02 +0900

    modify: build - install package-lock.json add

--- src/modules/builder/nest.ts
+++ src/modules/builder/nest.ts
@@ -1,5 +1,7 @@
 import { ChildProcess, exec as childExec  } from 'child_process';
 import CommandBuilder from '../common/commandBuilder';
+import fs from 'fs';
+
 const NestBuilder = function (this: any, config: any): any {
   const { appLocation } = config;
   function exec() {
@@ -7,17 +9,25 @@ const NestBuilder = function (this: any, config: any): any {
       const Command = new CommandBuilder();
       const buildCommand = `cd ${appLocation} && nest build`;
       Command.add(buildCommand);
+
+      const existPackageLockFile = fs.existsSync(`${appLocation}/package-lock.json`);
+      if(!existPackageLockFile) {
+        const addLockFile = `cd ${appLocation} && npm install --package-lock-only`;
+        Command.add(addLockFile);
+      }
+      
       const initCmd = Command.getCommand();
       const buildChild: ChildProcess = childExec(initCmd);
-        buildChild.on('close', () => {
-          console.log('finished building, please check your build folder!');
-          resolve(true);
-        })
-        buildChild.on('error', (err) => {
-          reject("nest build failed" + err);
-        })
+      
+      buildChild.on('close', () => {
+        console.log('finished building, please check your build folder!');
+        resolve(true);
+      })
+      buildChild.on('error', (err) => {
+        reject("nest build failed" + err);
       });
-    }
+    });
+  }
 
   return { exec };
 };
--- src/modules/builder/next.ts
+++ src/modules/builder/next.ts
@@ -1,5 +1,6 @@
 import childProcess from 'child_process';
 import CommandBuilder from '../common/commandBuilder';
+import fs from 'fs';
 
 const NextBuilder = function (this: any, config: any = {}): any {
   const { appLocation } = config;
@@ -9,6 +10,12 @@ const NextBuilder = function (this: any, config: any = {}): any {
       const appBuild = `cd ${appLocation} && npx next build`;
       const command = new CommandBuilder();
       command.add(appBuild);
+
+      const existPackageLockFile = fs.existsSync(`${appLocation}/package-lock.json`);
+      if(!existPackageLockFile) {
+        const addLockFile = `cd ${appLocation} && npm install --package-lock-only`;
+        command.add(addLockFile);
+      }
 
       const cmd = command.getCommand();
 
